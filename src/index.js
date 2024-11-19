9f70e262c2d83b3ba246c40e6784a7a63d12f758
ahmadakbar03 <akbarvideoeditor03@gmail.com>
2024-11-19 23:13:14 +0700

    1.1 Changes

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from './pages/App';
+import reportWebVitals from './help/reportWebVitals';
+import { BrowserRouter as Router } from 'react-router-dom';
+import './styles/index.css';
+import './styles/global.css';
+import './styles/responsive.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>
 );
 
---
+++ src/pages/general/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <section className="container">
+
+    </section>
+  );
+}
+
+export default App;
