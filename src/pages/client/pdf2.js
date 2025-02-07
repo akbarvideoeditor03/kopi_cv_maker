25a72d606f4091c22687e9d500204193e5074169
ahmadakbar03 <akbarvideoeditor03@gmail.com>
2025-02-07 17:42:44 +0700

    12.91.4 Changes

--- src/pages/client/pdf2.js
+++ src/pages/client/pdf2.js
@@ -15,7 +15,7 @@ const HTMLToPDF = () => {
             width: 170,
             windowWidth: 800,
             margin:20,
-            // autoPaging:"text"
+            autoPaging:"text"
         });
     };
 
@@ -23,7 +23,7 @@ const HTMLToPDF = () => {
         <div className="container col-f card-mini">
             <div ref={contentRef} style={{ padding: "1rem", textAlign:"justify" }}>
                 <p>
-                        <strong>Lorem ipsum</strong> dolor <i>sit amet</i> consectetur <u>adipisicing elit</u>. Repellat quae minus totam eum recusandae corporis pariatur expedita aspernatur animi voluptas possimus facere illum quis dolorem quaerat atque adipisci, cupiditate ad.
+                        <b>Lorem ipsum</b> dolor <i>sit amet</i> consectetur <u>adipisicing elit</u>. Repellat quae minus totam eum recusandae corporis pariatur expedita aspernatur animi voluptas possimus facere illum quis dolorem quaerat atque adipisci, cupiditate ad.
                         <ul>
                             <li>Lorem Ipsum</li>
                             <li>Lorem Ipsum</li>
@@ -31,22 +31,22 @@ const HTMLToPDF = () => {
                             <li>Lorem Ipsum</li>
                         </ul>
                         <p>
-                            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+                            "Lorem ipsum dolor sit amet, c<b style={{ marginTop:'-10px' }}>Lorem ipsum</b>onsectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+                            Section 1.10.32 of "de Finibus Bonorum et Malorum", written by Cicero in 45 BC
+                        </p>
+                        <br/>
+                        <p>
+                            "Lorem ipsum dolor sit amet, c<b style={{ marginTop:'-10px' }}>Lorem ipsum</b>onsectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
                             Section 1.10.32 of "de Finibus Bonorum et Malorum", written by Cicero in 45 BC
                         </p>
                         <br/>
                         <p>
                             "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-                            Section 1.10.32 of "de Finibus Bonorum et Malorum", written by Cicero in 45 BC
+                            Section 1.10.32 of "de Finibus Bonorum<b>Lorem ipsum</b> et Malorum", written by Cicero in 45 BC
                         </p>
                         <br/>
                         <p>
-                            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-                            Section 1.10.32 of "de Finibus Bonorum et Malorum", written by Cicero in 45 BC
-                        </p>
-                        <br/>
-                        <p>
-                            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+                            "Lorem ipsum dolor sit amet, c<b>Lorem ipsum</b>onsectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
                             Section 1.10.32 of "de Finibus Bonorum et Malorum", written by Cicero in 45 BC
                         </p>
                         <br/>
