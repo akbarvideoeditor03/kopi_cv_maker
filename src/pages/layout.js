370d21a0e67f4e6700912edc5101b6dd48fcecb8
ahmadakbar03 <akbarvideoeditor03@gmail.com>
2025-07-04 21:58:59 +0700

    14.3 Changes - Add Google Ads

--- src/pages/layout.js
+++ src/pages/layout.js
@@ -2,6 +2,7 @@ import React, {useEffect} from 'react';
 import { Outlet } from 'react-router-dom';
 import WebHeader from './web-component/header';
 import WebFooter from './web-component/footer';
+import AdHeader from './web-component/ads';
 
 function Layout() {
     const darkMode = localStorage.getItem('dark-mode');
@@ -12,10 +13,20 @@ function Layout() {
         } else {
             darkPage.classList.remove('dark-mode');
         }
-    }, [])
+    }, []);
+
+    useEffect(() => {
+        const script = document.createElement('script');
+        script.src = 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2846989538167510';
+        script.async = true;
+        script.crossOrigin = 'anonymous';
+        document.head.appendChild(script);
+    }, []);
+
     return (
         <section id='page' className='dark-mode'>
             <WebHeader />
+                <AdHeader />
                 <Outlet />
             <WebFooter />
         </section>
---
+++ src/pages/web-component/ads.js
@@ -0,0 +1,24 @@
+// src/web-component/AdHeader.js
+import { useEffect } from 'react';
+
+function AdHeader() {
+    useEffect(() => {
+        try {
+            (window.adsbygoogle = window.adsbygoogle || []).push({});
+        } catch (err) {
+            console.error('AdSense error', err);
+        }
+    }, []);
+
+    return (
+        <ins className="adsbygoogle"
+            style={{ display: 'block' }}
+            data-ad-client="ca-pub-2846989538167510"
+            data-ad-slot="9585066112"
+            data-ad-format="auto"
+            data-full-width-responsive="true">
+        </ins>
+    );
+}
+
+export default AdHeader;
